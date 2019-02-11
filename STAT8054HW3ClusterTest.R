4bf9ffd746a65c98e4bc5f62684f2b08abe0c90e
GHilleren <47537621+GHilleren@users.noreply.github.com>
2019-02-11 16:50:18 -0600

    Add files via upload

---
+++ STAT8054HW3ClusterTest.R
@@ -0,0 +1,32 @@
+#Unix Forking Method
+##Define the function.
+doit <- function(nsim, estimator) {
+  result <- double(nsim)
+  for (i in 1:nsim) {
+    x <- rnorm(n, theta, abs(theta))
+    result[i] <- estimator(x)
+  }
+  return(result)
+}
+
+##Run the function.
+mout <- mclapply(rep(nsim / ncores, ncores), doit,
+                 estimator = mle, mc.cores = ncores)
+lapply(mout, head)
+
+##Plot it.
+theta.hat <- unlist(mout)
+hist(theta.hat, probability = TRUE, breaks = 30)
+curve(dnorm(x, mean = theta, sd = theta / sqrt(3 * n)), add = TRUE)
+
+
+
+#Cluster method.
+##Set up.
+clusterExport(cl, c("doit", "mle", "mlogl", "n", "nsim", "theta"))
+
+##Run it.
+pout <- parLapply(cl, rep(nsim / ncores, ncores), doit, estimator = mle)
+
+##Stop the cluster.
+stopCluster(cl)
