aa81e641998156378432792817a359c93ad8f2b2
GHilleren <47537621+GHilleren@users.noreply.github.com>
2019-02-12 12:13:58 -0600

    Add files via upload

--- STAT8054HW3ClusterTest.R
+++ STAT8054HW3ClusterTest.R
@@ -1,12 +1,28 @@
 #Load library needed.
 library(parallel)
+
+#Define values and the seed.
 ncores <- detectCores()
 n <- 10
 nsim <- 1e4
 theta <- 1
+RNGkind("L'Ecuyer-CMRG")
+set.seed(42)
 
 #Unix Forking Method
-##Define the function.
+##Define the functions.
+mlogl <- function(theta, x) sum(- dnorm(x, theta, abs(theta), log = TRUE))
+
+mle <- function(x) {
+  theta.start <- sign(mean(x)) * sd(x)
+  if (all(x == 0) || theta.start == 0)
+    return(0)
+  nout <- nlm(mlogl, theta.start, iterlim = 1000, x = x)
+  if (nout$code > 3)
+    return(NaN)
+  return(nout$estimate)
+}
+
 doit <- function(nsim, estimator) {
   result <- double(nsim)
   for (i in 1:nsim) {
